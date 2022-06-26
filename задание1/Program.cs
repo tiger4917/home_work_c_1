ea1147c43fe79708bce914f1867cbd7339a7ba08
tiger4917 <tiger4917@mail.ru>
2022-06-26 16:15:38 +0300

    cделал задачу 1

---
+++ задание1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("ВВедите число");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("ВВедите число");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+int maxNumber = firstNumber;
+
+
+if(secondNumber > maxNumber)
+{
+maxNumber = secondNumber;
+}
+Console.WriteLine($"Максимальное значение = {maxNumber} ");
+
+
+
