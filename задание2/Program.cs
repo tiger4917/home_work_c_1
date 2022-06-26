7d8062de88bb237e3d9536d85daf746155127043
tiger4917 <tiger4917@mail.ru>
2022-06-26 17:52:27 +0300

    задание 2

---
+++ задание2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число1");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число2");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число3");
+int firdNumber = Convert.ToInt32(Console.ReadLine());
+int maxNumber = firstNumber;
+if (secondNumber > maxNumber)
+{
+maxNumber = secondNumber;
+}
+if (firdNumber > maxNumber)
+{
+maxNumber = firdNumber;
+}
+Console.WriteLine($"Максимальное значение = {maxNumber} ");
