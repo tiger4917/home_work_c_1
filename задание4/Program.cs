4cae531ea251325c46719c324c0b874fa56b9df9
tiger4917 <tiger4917@mail.ru>
2022-06-28 15:00:34 +0300

    Добавили 4 и 5 задание

---
+++ задание4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+for (count =1; count < n; count ++)
+{
+     if (count % 2==0)
+     {
+          Console.WriteLine($"{count}");
+     }
+}
---
+++ задание5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите трехзначное число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 100 && number <1000)
+{
+int firstNumber = number % 100;
+int secondNumber = firstNumber / 10;
+Console.WriteLine($"{number} -> {secondNumber}");
+}
+else 
+{
+Console.WriteLine("Неверно введено");
+}
