385b187a83c6077cf4aa925bcb1d9bfa50df7dc5
tiger4917 <tiger4917@mail.ru>
2022-06-30 14:49:24 +0300

    добавил задание6

---
+++ задание6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число :");
+string a = Console.ReadLine();
+if (a.Length >2)
+{
+Console.WriteLine($"{a}-> {a[2]}");
+}
+else 
+{
+    Console.WriteLine("Третьей цифры не существует");
+}
