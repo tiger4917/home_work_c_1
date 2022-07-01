7df00a68f633dc0e6ed5f187882ece28a012d6f2
tiger4917 <tiger4917@mail.ru>
2022-07-01 12:18:58 +0300

    добавил задание7

---
+++ задание7/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 8)
+{
+    if (number == 6 || number == 7)
+    {
+        Console.WriteLine("Сегодня выходной");
+    }
+    else
+    {
+        Console.WriteLine("Сегодня рабочий день");
+    }
+}
+else
+{
+    Console.WriteLine(" в неделе 7 дней ");
+}
