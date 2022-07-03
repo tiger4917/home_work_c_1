ae966ded3e25e34fde040d092c8f6bd68b149676
tiger4917 <tiger4917@mail.ru>
2022-07-03 18:12:09 +0300

    Добавил задание9 03_07

---
+++ задание9_03_07/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число из 5 цифер: ");
+string n = Console.ReadLine();
+if (n.Length == 5)
+{
+    if (n[0] == n[4] || n[1] == n[3])
+    {
+        Console.WriteLine("Число является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine("Число не является палиндромом");
+    }
+}
+else
+{
+    Console.WriteLine("неправильный ввод");
+}
