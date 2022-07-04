ecfd423f62ca7b51f952eae78a20b0b8fae189a1
tiger4917 <tiger4917@mail.ru>
2022-07-04 20:32:32 +0300

    добавил №23

---
+++ задание10(23)/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача №23 про возведение числа в натуральную степень через цикл
+Console.WriteLine("Введите число А:  и введите желаемую степень B:");
+double[] numbers = new double[2];  // 
+for (int i = 0; i < 2; i++)
+{
+    numbers[i] = Convert.ToInt32(Console.ReadLine());
+}
+double result = Math.Pow(numbers[0], numbers[1]);
+
+Console.WriteLine($"число {numbers[0]} , {numbers[1]} результат:{result}");
