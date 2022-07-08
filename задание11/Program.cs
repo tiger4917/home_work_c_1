73b74e37e92cc8f33b7f376ce1d39209dfe3bea3
tiger4917 <tiger4917@mail.ru>
2022-07-08 07:50:51 +0300

    добавил задание11

---
+++ задание11/Program.cs
@@ -0,0 +1,18 @@
+﻿//: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+while ( a>0)
+{
+    result += a%10;
+    a= a/10;
+}
+Console.WriteLine(result);
+
+
+
+
+
+
+
+
