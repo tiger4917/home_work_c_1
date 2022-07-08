24dcd3e85f358ff8ba6a6984960e0c82a616272e
tiger4917 <tiger4917@mail.ru>
2022-07-08 10:38:02 +0300

    добавил задание 13

---
+++ задание13/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая принимает на вход число (N)
+//и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Input Number: ");
+double num = Convert.ToInt32(Console.ReadLine());
+
+for (int i =1; i<=num ; i ++ )
+{
+    double result = Math.Pow(i, 3);
+    Console.Write( result + "  ");
+}
