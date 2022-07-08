3c292512d0a3552c80675273624731807a2912d5
tiger4917 <tiger4917@mail.ru>
2022-07-08 12:21:09 +0300

    добавил задание 14(21)

---
+++ задание12(29)/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая задаёт массив
+// из 8 элементов и выводит их на экран.
+int[] array = new int[8];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1,10);
+    Console.Write(array[i] + ",");
+}   
+
---
+++ задание14(21)/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 3D пространстве.  
+// A (3,6,8); B (2,1,-7), -> 15.84
+int x1 = InputInt("Введите X1: ");
+int y1 = InputInt("Введите y1: ");
+int z1 = InputInt("Введите z1: ");
+
+int x2 = InputInt("Введите X2: ");
+int y2 = InputInt("Введите y2: ");
+int z2 = InputInt("Введите z2: ");
+
+int distanceX = x2 - x1;
+int distanceY = y2 - y1;
+int distanceZ = z2 - z1;
+
+double distance = Math.Sqrt(distanceX * distanceX + distanceY * distanceY + distanceZ * distanceZ);
+Console.WriteLine(distance);
+
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
