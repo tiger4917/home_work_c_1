a655c8bd535844539fe375daf6c9bcf2d14ede96
tiger4917 <tiger4917@mail.ru>
2022-07-09 18:21:39 +0300

    добавил задание 15

---
+++ задание15/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами.
+//Напишите программу,
+//которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+int[] array = new int[8];
+int result = 0;
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 999);
+    Console.Write(array[i] + " ");
+    if (array[i] % 2 == 0)
+    {
+        result += 1;
+    }
+}
+Console.Write("] ->" +result);
