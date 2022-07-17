884d2f0437411312f5d730a667a6d0f7c75ab78d
tiger4917 <tiger4917@mail.ru>
2022-07-17 11:17:30 +0300

    добавил задание 20

---
+++ задание20/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задайте двумерный массив размером m×n,
+// заполненный случайными вещественными числами.
+
+int[,] array = new int[3, 4];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(1, 100);
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
